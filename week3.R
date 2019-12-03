cbb192e717577d70cb316367e4051a609c68fa81
leema-bastinleo <53094910+leema-bastinleo@users.noreply.github.com>
2019-12-03 21:36:12 +0000

    Add files via upload

---
+++ week3.R
@@ -0,0 +1,33 @@
+## Caching the inverse of a matrix
+
+## function to set a matrix and its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}
+
+
+## function to solve for a matrix's inverse if it has not been cached previously.
+
+cacheSolve <- function(x, ...) {
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+}
+     
